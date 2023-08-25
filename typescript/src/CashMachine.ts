efb504261bb0c807646157c65f2d47a095b478fc
Joanna Liana <joanna_liana@outlook.com>
2023-08-25 09:24:29 +0200

    Implement the new requirement in the old design

--- typescript/src/CashMachine.ts
+++ typescript/src/CashMachine.ts
@@ -3,15 +3,18 @@ import { BranchFinder } from './BranchFinder';
 export class CashMachine {
   private readonly branchFinder = new BranchFinder();
 
-  withdraw({ cashAmount, customerName, town }: {
+  // a change in PersonalAccountsManager
+  // enforced a change to the interface of CashMachine
+  withdraw({ cashAmount, customerName, bankManager, town }: {
     town: string;
     customerName: string;
+    bankManager: string;
     cashAmount: number;
   }): boolean {
     return this.branchFinder
       .findBranchForTown(town)
       .personalAccounts
-      .getAccountForCustomer(customerName)
+      .getAccountForCustomer(customerName, bankManager)
       .withdraw(cashAmount);
   }
 }
